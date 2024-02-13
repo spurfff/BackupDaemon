771eb9a128312ee3b4b3cf4c151758e2b7673728
spurfff <spencerprfoster@outlook.com>
2024-02-12 19:22:26 -0500

    even more error checking and clean up

--- backup_nectar_logs.sh
+++ backup_nectar_logs.sh
@@ -7,28 +7,28 @@ red='\033[0;31m'
 green='\033[0;32m'
 reset='\033[0m'
 bold='\033[1;37m'
-remote_dir="/Backup/remote_dir_Dir"
-local_dir="/Backup/local_dir_Dir"
-logprefix=""
+remote_dir="/$working_dir/Remote_Dir"
+local_dir="/$working_dir/Local_Dir"
+logprefix="${bold}$(date)${reset}"
 remote_server="10.37.3.16"
 
 # Make sure that the Backup directory and .log file are in place as expected
 if [[ -d "$working_dir" ]]; then
-	echo -e "Found $working_dir ${bold}$(date)${reset}\n" >> "${logs}"
+	echo -e "$logprefix Found $working_dir\n" >> "${logs}"
 	if [[ -f "${logs}" ]]; then
-		echo -e "Found $logs ${bold}$(date)${reset}\n" >> "${logs}"
+		echo -e "$logprefix Found $logs\n" >> "${logs}"
 	else
-		echo -e "$logs not found ${bold}$(date)${reset}\n" >> "${logs}"
-		echo -e "Creating log file...${bold}$(date)${reset}\n" >> "${logs}"
+		echo -e "$logprefix $logs not found\n" >> "${logs}"
+		echo -e "$logprefix Creating log file...\n" >> "${logs}"
 		touch $logs
 		if [[ $? -ne 0 ]]; then
-			echo -e "An error has occured while trying to create the log file at $logs ${bold}$(date)${reset}" >> "${logs}"
+			echo -e "$logprefix An error has occured while trying to create the log file at $logs \n" >> "${logs}"
 			exit 1
 		fi
 	fi
 else
-	echo -e "$working_dir not detected...${bold}$(date)${reset}\n" >> "${logs}"
-	echo -e "Exiting with errors...${bold}$(date)${reset}\n" >> "${logs}"
+	echo -e "$logprefix $working_dir not detected...\n" >> "${logs}"
+	echo -e "$logprefix Exiting with errors...\n" >> "${logs}"
 	exit 1
 fi
 
@@ -37,10 +37,10 @@ fi
 # The actual syncing process
 rsync -av "${remote_dir}" "${local_dir}" >> "${logs}"
 if [[ $? -eq 0 ]]; then
-	echo -e "rsync backup ${green}completed${reset} on ${bold}$(date)${reset}\n" >> "${logs}"
+	echo -e "$logprefix rsync backup ${green}completed${reset}\n" >> "${logs}"
 else
-	echo -e "rsync backup ${red}failed${reset} on ${bold}$(date)${reset}\n" >> "${logs}"
+	echo -e "$logprefix rsync backup ${red}failed${reset}\n" >> "${logs}"
 fi
 local_dir_size=$(du -sh "${local_dir}" | cut -d " " -f1)
-echo -e "Current Backup Size: ${local_dir_size}\n" >> "${logs}"
+echo -e "$logprefix Current Backup Size: ${local_dir_size}\n" >> "${logs}"
 echo -e "#############END################\n\n" >> "${logs}"
